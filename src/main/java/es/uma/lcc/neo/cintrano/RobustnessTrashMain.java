27491ff215204cfbf59ae57f58f91ff43861a046
christian-cl <christiancintranolopez@gmail.com>
2016-12-03 22:47:24 +0100

    first commit

---
+++ src/main/java/es/uma/lcc/neo/cintrano/RobustnessTrashMain.java
@@ -0,0 +1,12 @@
+package es.uma.lcc.neo.cintrano;
+
+/**
+ * Created by Christian Cintrano on 03/12/2016.
+ * Test main class fot the robustness bi-objective shortest path problem learning
+ */
+public class RobustnessTrashMain {
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ src/main/java/es/uma/lcc/neo/cintrano/TestMain.java
@@ -0,0 +1,80 @@
+package es.uma.lcc.neo.cintrano;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.operator.impl.crossover.SBXCrossover;
+import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
+import org.uma.jmetal.operator.impl.selection.BinaryTournamentSelection;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.runner.AbstractAlgorithmRunner;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemUtils;
+import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+
+
+/**
+ * Created by Christian Cintrano on 03/12/2016.
+ * Test main class to test applications and files
+ */
+public class TestMain extends AbstractAlgorithmRunner {
+    public static void main (String [] args) throws FileNotFoundException {
+        System.out.println("Hello world");
+
+        Problem<DoubleSolution> problem;
+        Algorithm<List<DoubleSolution>> algorithm;
+        CrossoverOperator<DoubleSolution> crossover;
+        MutationOperator<DoubleSolution> mutation;
+        SelectionOperator<List<DoubleSolution>, DoubleSolution> selection;
+        String referenceParetoFront = "" ;
+
+        String problemName ;
+        if (args.length == 1) {
+            problemName = args[0];
+        } else if (args.length == 2) {
+            problemName = args[0] ;
+            referenceParetoFront = args[1] ;
+        } else {
+            problemName = "org.uma.jmetal.problem.multiobjective.zdt.ZDT1";
+            referenceParetoFront = "src/main/resources/ZDT1.pf" ;
+        }
+
+        problem = ProblemUtils.<DoubleSolution> loadProblem(problemName);
+
+        double crossoverProbability = 0.9 ;
+        double crossoverDistributionIndex = 20.0 ;
+        crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex) ;
+
+        double mutationProbability = 1.0 / problem.getNumberOfVariables() ;
+        double mutationDistributionIndex = 20.0 ;
+        mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex) ;
+
+        selection = new BinaryTournamentSelection<DoubleSolution>(new RankingAndCrowdingDistanceComparator<DoubleSolution>());
+
+        algorithm = new NSGAIIBuilder<DoubleSolution>(problem, crossover, mutation)
+                .setSelectionOperator(selection)
+                .setMaxIterations(250)
+                .setPopulationSize(100)
+                .build() ;
+
+        AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm).execute() ;
+
+        List<DoubleSolution> population = algorithm.getResult() ;
+        long computingTime = algorithmRunner.getComputingTime() ;
+
+        JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+
+        printFinalSolutionSet(population);
+        if (!referenceParetoFront.equals("")) {
+            printQualityIndicators(population, referenceParetoFront) ;
+        }
+    }
+}
